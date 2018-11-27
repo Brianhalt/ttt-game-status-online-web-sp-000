7230751589872f6f435df9e5926869c8efb216e6
Brian Haltiwanger <bhalt@bellsouth.net>
2018-11-27 20:46:35 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,7 @@ WIN_COMBINATIONS = [
   [2,5,8]  # Right column
 ]
 def won? (board) # asignes each winning combination an index
-  WIN_COMBINATIONS.each do {|win_combination|
+  WIN_COMBINATIONS.each do |win_combination|
   # for each win_combination in WIN_COMBINATIONS
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
@@ -25,17 +25,16 @@ def won? (board) # asignes each winning combination an index
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
 
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    if full?(board) == false
+      return false
+    elsif position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination  #return the win_combination indexes that won.
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination  #return the win_combination indexes that won.
-    #elsif full?(board) == false
-    #  false
-    #else
-    #  false
+    else
+      false
     end
-  }
-  return false
+  end  
 end
 
 def full? (board)
