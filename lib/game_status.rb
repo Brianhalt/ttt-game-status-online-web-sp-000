4e3015a10f71ba9831e92ecf0c00b0bd96a10705
Brian Haltiwanger <bhalt@bellsouth.net>
2018-11-27 21:18:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -62,7 +62,7 @@ def draw? (board)
 end
 
 def over? (board)
-  if won?(board) == true || draw?(board) == true || full?(board) == true
+  if won?(board) == true || full?(board) == true
     return true
   else
     return false
