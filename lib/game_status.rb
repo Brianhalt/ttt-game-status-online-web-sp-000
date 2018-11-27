22204099904e96fd354daefd7c02137969e0d45d
Brian Haltiwanger <bhalt@bellsouth.net>
2018-11-27 20:53:13 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,13 +25,12 @@ def won? (board) # asignes each winning combination an index
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
 
-
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    if full?(board) == false
+      return false
+    elsif position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination  #return the win_combination indexes that won.
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination  #return the win_combination indexes that won.
-    elsif full?(board) == false
-        return false
     else
       false
     end
